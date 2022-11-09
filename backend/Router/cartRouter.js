f4db2111f97765385fb6b857f1cc416a74a37c49
user <manishchouhan2396@gmail.com>
2022-11-09 19:33:51 +0530

    add

--- backend/Router/cartRouter.js
+++ backend/Router/cartRouter.js
@@ -20,7 +20,7 @@ cartRouter.post("/addCart" , async(req , res) => {
 cartRouter.get("/getCart" ,  async(req , res) => {
     console.log(req.body.user_id)
     try{
-        const data = await cartModel.find({user_id:req.body.user_id});
+        const data = await cartModel.find();
         res.status(200).send({ user_id:req.body.user_id , data:data})
     }
     catch(error){
