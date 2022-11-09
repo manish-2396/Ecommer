bf1b7a7487b63cce8452fd2d0361786ca0dae380
user <manishchouhan2396@gmail.com>
2022-11-09 20:42:12 +0530

    getCart part 2

--- backend/middleware/authtication.js
+++ backend/middleware/authtication.js
@@ -3,9 +3,9 @@ const jwt = require('jsonwebtoken')
 // console.log(process.env.Code)
 
 const authtication = (req , res , next) => {
-    // console.log("manish" , req.headers.authorization)
+    console.log("manish" , req.headers.authorization)
     if(!req.headers.authorization){
-        res.status(500).send({massage:"User not authorized"})
+        res.status(500).send({massage:"User not authorized 1"})
     }else{
         const token = req.headers.authorization.split(" ")[1];
         jwt.verify(token , process.env.Code , (err , decoded) => {
