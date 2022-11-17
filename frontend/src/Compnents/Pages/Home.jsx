98f7ab32463d01bf8223eaa0c730094888096ffc
user <manishchouhan2396@gmail.com>
2022-11-17 22:15:01 +0530

    add

--- frontend/src/Compnents/Pages/Home.jsx
+++ frontend/src/Compnents/Pages/Home.jsx
@@ -4,16 +4,16 @@ import React from 'react'
 const Home = () => {
   return (
     <Container>
-      <Box m="auto" maxWidth="50%" fontSize="2rem" fontFamily="monospace" color="red" ><h3>Top Categories to choose from</h3></Box>
+      <Box m="auto" maxWidth="50%" fontSize="2rem" fontFamily="monospace" color="#323332" ><h3>Top Categories to choose from</h3></Box>
 
-      <Box border="1px solid black" maxHeight="30rem" display="flex" gap="1rem">
-        <Box>
+      <Box  maxHeight="auto" display="flex" justifyContent="space-between" p="1.5rem" flexWrap="wrap" id="backgrond-img" >
+        <Box m="auto">
           <img src="https://images.meesho.com/images/marketing/1649760442043.webp" alt="" />
-        </Box>
-        <Box>
+        </Box >
+        <Box m="auto">
           <img src="https://images.meesho.com/images/marketing/1649760423313.webp" alt="" />
-        </Box>
-        <Box>
+        </Box >
+        <Box m="auto">
           <img src="https://images.meesho.com/images/marketing/1649759799809.webp" alt="" />
         </Box>
       </Box>
